94bddd78cc16a25c17f5b9dc48ecdf557a4e120f
podsnigame <124865656+podsnigame@users.noreply.github.com>
2023-02-18 12:31:00 +0700

    Update aipushtes.sh

--- script/aipushtes.sh
+++ script/aipushtes.sh
@@ -4,7 +4,7 @@
 git add *
 
 # Membuat commit message secara otomatis dengan AI
-commit_message=$(curl -X POST -H "Authorization: Bearer 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" -H "Content-Type: application/json" -d '{"prompt": "Please enter the commit message for the code changes:"}' "https://api.openai.com/v1/engines/davinci-codex/completions" | jq -r '.choices[].text')
+commit_message=$(curl -X POST -H "Authorization: Bearer" -H "Content-Type: application/json" -d '{"prompt": "Please enter the commit message for the code changes:"}' "https://api.openai.com/v1/engines/davinci-codex/completions" | jq -r '.choices[].text')
 
 # Melakukan git commit dengan message yang telah dibuat secara otomatis
 git commit -m "$commit_message"
